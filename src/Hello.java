41e57a49e7891bf456305e6a2ff1fb9b1a9cb682
Jemy Dalsaniya <jemy.inexture@gmail.com>
2022-06-14 10:56:01 +0530

    stream api

---
+++ src/Hello.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(12,15,13,14,10);
+
+
+        System.out.println("list is"+list);
+
+       //Stream<Integer> stream = list.stream();
+        List<Integer> evenList = list.stream().filter(integer -> integer%2==0).collect(Collectors.toList());
+        List<Integer> maxList = list.stream().filter(i -> i>10).collect(Collectors.toList());
+
+        Stream<Object> emptyStream = Stream.empty();
+        String name[] = {"Jemy","Riddhi","Shivu","abc"};
+        Stream<String> names = Stream.of(name);
+        names.forEach(e->{
+            System.out.println(e);
+        });
+
+       // Arrays.stream(name).forEach(System.out::println);
+        //name.stream().forEach(System.out::println);
+        Arrays.stream(name).sorted().forEach(System.out::println);
+
+//        System.out.println("even list"+evenList);
+//        System.out.println("max list"+maxList);
+    }
+}
